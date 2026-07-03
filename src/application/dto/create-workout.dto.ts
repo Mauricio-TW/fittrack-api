773ff9309087cf61f25948314dd847b5e1d0dffb
douglasrodrigues528 <douglas.rodrigues.granprime@gmail.com>
2026-07-03 02:11:03 -0300

    feat: adiciona exercícios, séries, peso e repetições

--- src/application/dto/create-workout.dto.ts
+++ src/application/dto/create-workout.dto.ts
@@ -1,9 +1,53 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
+  IsArray,
   IsNotEmpty,
+  IsNumber,
   IsOptional,
   IsString,
+  ValidateNested,
 } from 'class-validator';
+import { Type } from 'class-transformer';
+
+class WorkoutSetDto {
+  @ApiProperty({
+    example: 40,
+  })
+  @IsNumber()
+  weight!: number;
+
+  @ApiProperty({
+    example: 12,
+  })
+  @IsNumber()
+  reps!: number;
+}
+
+class WorkoutExerciseDto {
+  @ApiProperty({
+    example: 'Supino reto',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+
+  @ApiProperty({
+    example: [
+      {
+        weight: 40,
+        reps: 12,
+      },
+      {
+        weight: 45,
+        reps: 10,
+      },
+    ],
+  })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => WorkoutSetDto)
+  sets!: WorkoutSetDto[];
+}
 
 export class CreateWorkoutDto {
   @ApiProperty({
@@ -20,4 +64,28 @@ export class CreateWorkoutDto {
   @IsString()
   @IsOptional()
   description?: string;
+
+  @ApiProperty({
+    required: false,
+    example: [
+      {
+        name: 'Supino reto',
+        sets: [
+          {
+            weight: 40,
+            reps: 12,
+          },
+          {
+            weight: 45,
+            reps: 10,
+          },
+        ],
+      },
+    ],
+  })
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => WorkoutExerciseDto)
+  exercises?: WorkoutExerciseDto[];
 }
--- src/infra/database/entities/workout.entity.ts
+++ src/infra/database/entities/workout.entity.ts
@@ -8,6 +8,14 @@ import {
 
 import { UserEntity } from './user.entity';
 
+export interface WorkoutExercise {
+  name: string;
+  sets: {
+    weight: number;
+    reps: number;
+  }[];
+}
+
 @Entity('workouts')
 export class WorkoutEntity {
   @PrimaryGeneratedColumn('uuid')
@@ -20,6 +28,13 @@ export class WorkoutEntity {
     nullable: true,
   })
   description!: string;
+
+  @Column({
+    type: 'jsonb',
+    nullable: true,
+    default: [],
+  })
+  exercises!: WorkoutExercise[];
 
   @ManyToOne(() => UserEntity, (user) => user.workouts)
   user!: UserEntity;
